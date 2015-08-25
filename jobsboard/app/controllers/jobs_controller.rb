adebaef8dcf8dc938cb128d5a5316c9e45b2c618
hilpitome <hilpitome@gmail.com>
2015-08-25 13:36:34 +0300

    defined index in controller

--- jobsboard/app/controllers/jobs_controller.rb
+++ jobsboard/app/controllers/jobs_controller.rb
@@ -1,12 +1,13 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
 
   def new
      @job = Job.new
   end
   def create
-    @job = Job.new
+    @job = Job.new(params.require(:job).permit(:title, :company, :url))
     if @job.save
     redirect_to root_path
     else
